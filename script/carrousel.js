50007267b09499664ee1a817688aee502556122f
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-28 23:57:26 -0400

    Modification du script js de mon extension

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,21 +1,19 @@
 (function () {
 
-console.log('ui');
+    console.log('ui');
 
-// Création dynamique d'une galerie.
+    // Création dynamique d'une galerie.
 
-let elGalerie = document.querySelector('.galerie'); // Récuperation de galerie créé dans l'éditeur gutemberg 
-let elGalerie__img = elGalerie.querySelectorAll('.galerie figure img'); // Récuperation des images de la galerie gutemberg
-
-
-for (let i = 0; i < elGalerie__img.length; i++) {
-    elGalerie__img[i].addEventListener('mousedown', function () {  //Image clické ? : on accède récupère son 'scr';
-        let src = elGalerie__img[i].getAttribute('src');
-
-        console.log(src);
-    });
-}
+    let elGalerie = document.querySelector('.galerie'); // Récuperation de galerie créé dans l'éditeur gutemberg 
+    let elGalerie__img = elGalerie.querySelectorAll('.galerie figure img'); // Récuperation des images de la galerie gutemberg
 
 
+    for (let i = 0; i < elGalerie__img.length; i++) {
+        elGalerie__img[i].addEventListener('mousedown', function () {  //Image clické ? : on accède récupère son 'scr';
+            let src = elGalerie__img[i].getAttribute('src');
+
+            console.log(src);
+        });
+    }
 
 })();
