e0861006c257d9fad290a5bbd817932492a43f34
kennethEdwin147 <edwingbeti@gmail.com>
2022-07-06 01:17:29 -0400

    semaine2-s2 Modification fichier carrousel.php

--- carrousel.php
+++ carrousel.php
@@ -3,47 +3,40 @@
  * Plugin Name: Carrousel
  * Author: kenneth Gbeti
  * Description: Carrousel d'images créé à partir d'une galerie d'image.
- * Author URI: https://github.com/kennethEdwin147/31w-extension-exer2
+ * Author URI: https://github.com/kennethEdwin147/31w-extension-tp1
  */ 
-function carrousel_31w_enqueue() 
+function carrousel_31w_enqueue()
 {
-    $version_css = filemtime(plugin_dir_path(__FILE__) . 'style.css');
-    $version_js =  filemtime(plugin_dir_path(__FILE__) . 'script/carrousel.js');
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "script/carrousel.js");
 
-    wp_enqueue_style(   
-                    'carrousel_31w_enqueue', 
-                    plugin_dir_url(__FILE__) . 'style.css',
-                    array(),
-                    $version_css 
-                );
+    wp_enqueue_style(   'carrousel_31w_css', 
+                        plugin_dir_url( __FILE__)  . "style.css",
+                        array(),
+                        $version_css);
 
-
-    wp_enqueue_script(
-                    'carrousel_31w_enqueue', 
-                    plugin_dir_url(__FILE__) . 'script/carrousel.js',
-                    array(),
-                    $version_js,
-                    true 
-                );
+    wp_enqueue_script(  'carrousel_31w_js', 
+                        plugin_dir_url( __FILE__) . "script/carrousel.js",
+                        array(),
+                        $version_js,
+                        true);
 }
-
 add_action('wp_enqueue_scripts', 'carrousel_31w_enqueue');
 
-function genere_carrousel() 
-{
-    ///////////////////HTML
+function genere_carrousel(){
+    /////////////////////////////////////// HTML
+    // Le conteneur du carrousel
     $contenu = '<div class="carrousel">';
+    // Le bouton fermeture
     $contenu .= '<button class="carrousel__fermeture">X</button>';
-
-    $contenu .= '<figure class="carrousel__img">';
-    $contenu .= '</figure>';
-
-    $contenu .= '<form class="carrousel__radio">';
-    $contenu .= '</form>';
-
-    $contenu .= '</div> <!-- fin du car -->';
+    // Le conteneur d'images
+    $contenu .= '<figure class="carrousel__figure"></figure>';
+    // Le formulaire des radio bouton pour naviguer d'une image à l'autre
+    $contenu .= '<form class="carrousel__radio"></form>';
+    $contenu .= '</div> <!-- fin du carrousel -->';
+   
     return $contenu;
 }
 
-add_shortcode('carrousel','genere_carrousel');
+add_shortcode('carrousel', 'genere_carrousel');
 
